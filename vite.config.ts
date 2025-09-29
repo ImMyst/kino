d88cbae528caa8445607782db352b55c6e42d0fd
ImMyst <titouan.galvani@yeap.ai>
2025-09-29 20:52:39 +0200

    chore: remove devtools on production

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import netlify from "@netlify/vite-plugin-tanstack-start";
 import tailwindcss from "@tailwindcss/vite";
+import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+
 import viteTsConfigPaths from "vite-tsconfig-paths";
 
 const config = defineConfig({
@@ -14,6 +16,9 @@ const config = defineConfig({
     tailwindcss(),
     tanstackStart(),
     viteReact(),
+    devtools({
+      removeDevtoolsOnBuild: true
+    }),
     netlify(),
   ],
 });
